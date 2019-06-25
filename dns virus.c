4639c3db837535a50675fd23ffbe4f2c8c5092ce
蕉蕉バナナ <52216734+Vbanana@users.noreply.github.com>
2019-06-26 03:12:08 +0800

    Update dns virus.c

--- dns virus.c
+++ dns virus.c
@@ -3,28 +3,33 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <wchar.h>
+#include <locale.h>
 #include <io.h>
 #include <sys/stat.h>
 #define max 150
-int main()
+void main()
 {
-	printf("創作者by BT茶\n修改 by 蕉蕉\n");
-	int h=open("C:\\WINDOWS\\system32\\drivers\\etc\\hosts",O_CREAT|O_WRONLY|O_APPEND);
+	wchar_t myString1[16] = { 0x8549, 0x8549};
+	wchar_t myString2[16] = { 0x5275,0x4F5C,0x8005};
+	wchar_t myString3[16] = { 0x8336};
+	wchar_t myString4[16] = { 0x4FEE,0x6539};
+	setlocale(LC_ALL, "");
+	wprintf(L"%ls by BT%ls\n%ls by %ls\n",myString2,myString3,myString4,myString1);
+	int i,h=open("C:\\WINDOWS\\system32\\drivers\\etc\\hosts",O_WRONLY|O_APPEND);
 	char a[9]={"0.0.0.0 "},url[max],n='\n';
-	write(h,&n,1);
 	if(h!=-1)
-		while(1)
-		{
-			fflush(stdin);
-			printf("input url:");
-			scanf("%s",url);
-			if(url=="quit")
-			  break;
-			write(h,a,8);
-			write(h,url,strlen(url));
-			write(h,&n,1);
-		}	
+	while(1)
+	{
+		printf("input url:");
+		scanf("%s",url);
+		if(url=="quit")
+		  break;
+		write(h,a,8);
+		write(h,url,strlen(url));
+		write(h,&n,1);
+		fflush(stdin);
+	}	
 	close(h);
 	system("pause");
-	return 0;
 } 
